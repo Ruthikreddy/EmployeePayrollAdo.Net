a24747ea23d8c8342e9c9d9ffafe79ac7a693eb8
Ruthikreddy <ruthikreddy15@gmail.com>
2020-11-04 18:40:32 +0530

    [Ruthik] push for Connection

---
+++ EmployeeRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ADO.NetDemo
+{
+    public class EmployeeRepository
+    {
+        //public static string ConnectionString = @"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=payroll_services;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public static string ConnectionString = "Server=(localdb)\\MSSQLLocalDB;Database=Employee;Trusted_connection=True;MultipleActiveResultSets=True";
+        //public static string ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog =payroll_services; User ID = Ruthik; Password=Spacebar99*";
+        SqlConnection connection = new SqlConnection(ConnectionString);
+
+        public void GetAllEmployees()
+        {
+            EmployeeModel model = new EmployeeModel();
+            try
+            {
+                using (connection)
+                {
+                    string query = @"select * from dbo.employee_payroll";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    this.connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            model.EmployeeID = reader.GetInt32(0);
+                            model.EmployeeName = reader.GetString(1);
+                            model.BasicPay = reader.GetDouble(2);
+                            model.StartDate = reader.GetDateTime(3);
+                            model.Gender = reader.GetString(4);
+                            model.PhoneNumber = reader.GetString(5);
+                            model.Address = reader.GetString(6);
+                            model.Department = reader.GetString(7);
+                            model.Deductions = reader.GetDouble(8);
+                            model.TaxablePay = reader.GetDouble(9);
+                            model.NetPay = reader.GetDouble(10);
+                            model.Tax = reader.GetDouble(11);
+                            Console.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11}", model.EmployeeID, model.EmployeeName, model.Gender, model.Address, model.BasicPay, model.StartDate, model.PhoneNumber, model.Address, model.Department, model.Deductions, model.TaxablePay, model.Tax, model.NetPay);
+                            Console.WriteLine("\n");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No data found");
+                    }
+                    reader.Close();
+                    //this.connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ADO.NetDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+             EmployeeRepository repository = new EmployeeRepository();
+             repository.GetAllEmployees();
+            Console.ReadLine();
+        }
+    }
+}
