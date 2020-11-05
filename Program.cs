ffc657df17685ef0386c8f3ee83eda6e0f639ca9
Ruthikreddy <ruthikreddy15@gmail.com>
2020-11-05 20:14:10 +0530

    [Ruthik] Push for getting sum avg grouping by gender

--- Program.cs
+++ Program.cs
@@ -6,16 +6,6 @@ namespace ADO.NetDemo
     {
         static void Main(string[] args)
         {
-            //EmployeeRepository salary = new EmployeeRepository();
-            //SalaryUpdateModel updateModel = new SalaryUpdateModel()
-            //{
-            //    SalaryId = 1,
-            //    Month = "Jan",
-            //    EmployeeSalary = 120,
-            //    EmployeeId = 1
-            //};
-            //int EmpSalary = salary.UpdateSalary(updateModel);
-            //Console.WriteLine("return int {0}",salary.UpdateSalary(updateModel));
             EmployeeRepository repository = new EmployeeRepository();
             repository.GroupingDataToFindMinMaxSumAverage();
             Console.ReadLine();
